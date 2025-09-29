d91ff82e3eeaaa2c7b2744545a4250a3729223d0
Barbara  Samira Santiesteban Saravia <145080442+BarbaraSamiraS@users.noreply.github.com>
2025-09-29 12:14:29 -0300

    Delete js directory

--- js/login/login.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('.sign-in-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    window.location.href = "perfil.html";
-});
-document.getElementById('.sign-up-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    window.location.href = "perfil.html";
-});
